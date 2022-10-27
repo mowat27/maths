cf84da0b7f6b9c872eb901054d9106039bd2aad7
Adrian Mowat <adrian.mowat@gmail.com>
2022-10-28 00:29:59 +0100

    Print 0 by default

--- maths.rb
+++ maths.rb
@@ -2,13 +2,15 @@
 
 op = ARGV.shift
 
-case op
+puts case op
 when '+'
-  puts ARGV.reduce(0) { |result, n| result + n.to_i }
+  ARGV.reduce(0) { |result, n| result + n.to_i }
 when '-'
-  puts ARGV.reduce(0) { |result, n| result - n.to_i }
+  ARGV.reduce(0) { |result, n| result - n.to_i }
 when '/'
-  puts ARGV.reduce { |result, n| result.to_i / n.to_i unless n.to_i == 0 }
+  ARGV.reduce { |result, n| result.to_i / n.to_i unless n.to_i == 0 }
 when 'x'
-  puts ARGV.reduce(0) { |result, n| result * n.to_i }
+  ARGV.reduce(0) { |result, n| result * n.to_i }
+else
+  0
 end
