449bf0e550636f133eb12d1c8309423da0bdea07
Adrian Mowat <adrian.mowat@gmail.com>
2022-10-28 00:37:36 +0100

    Make a hash of functions

--- maths.rb
+++ maths.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 
+OPS = {
+  '+' => ->(numbers) { numbers.reduce(0) { |result, n| result + n.to_i } },
+  '-' => ->(numbers) { numbers.reduce(0) { |result, n| result - n.to_i } },
+  '/' => ->(numbers) { numbers.reduce { |result, n| result.to_i / n.to_i unless n.to_i == 0 } },
+  'x' => ->(numbers) { numbers.reduce(0) { |result, n| result * n.to_i } },
+}
+OPS.default = -> (numbers) { 0 }
+
 def maths(op, numbers)
-  case op
-  when '+'
-    numbers.reduce(0) { |result, n| result + n.to_i }
-  when '-'
-    numbers.reduce(0) { |result, n| result - n.to_i }
-  when '/'
-    numbers.reduce { |result, n| result.to_i / n.to_i unless n.to_i == 0 }
-  when 'x'
-    numbers.reduce(0) { |result, n| result * n.to_i }
-  end
+  OPS[op].call(numbers)
 end
 
 if __FILE__ == $0
