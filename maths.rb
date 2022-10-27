d2e046078c70afdc968e725f3332ba48ba18682d
Adrian Mowat <adrian.mowat@gmail.com>
2022-10-27 23:43:12 +0100

    Implement /

--- maths.rb
+++ maths.rb
@@ -10,4 +10,10 @@ elsif op == '-'
   result = 0
   ARGV.each { |n| result -= n.to_i }
   puts result
+elsif op == '/'
+  result = ARGV.shift.to_i
+  for n in ARGV
+    result = result / n.to_i unless n.to_i == 0
+  end
+  puts result
 end
