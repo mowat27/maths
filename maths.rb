91c8c7f8cfa89caf03fe5e135d90cd17958eb51a
Adrian Mowat <adrian.mowat@gmail.com>
2022-10-27 23:47:41 +0100

    Convert to switch statement

--- maths.rb
+++ maths.rb
@@ -2,21 +2,22 @@
 
 op = ARGV.shift
 
-if op == '+'
+case op
+when '+'
   result = 0
   ARGV.each { |n| result += n.to_i }
   puts result
-elsif op == '-'
+when '-'
   result = 0
   ARGV.each { |n| result -= n.to_i }
   puts result
-elsif op == '/'
+when '/'
   result = ARGV.shift.to_i
   for n in ARGV
     result = result / n.to_i unless n.to_i == 0
   end
   puts result
-elsif op == 'x'
+when 'x'
   result = ARGV.shift.to_i
   ARGV.each { |n| result *= n.to_i }
   puts result
