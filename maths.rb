afa85672c8f2b1a41a920e9be162646018b775f0
Adrian Mowat <adrian.mowat@gmail.com>
2022-10-28 00:28:28 +0100

    Use reduce everywhere

--- maths.rb
+++ maths.rb
@@ -6,17 +6,9 @@ case op
 when '+'
   puts ARGV.reduce(0) { |result, n| result + n.to_i }
 when '-'
-  result = 0
-  ARGV.each { |n| result -= n.to_i }
-  puts result
+  puts ARGV.reduce(0) { |result, n| result - n.to_i }
 when '/'
-  result = ARGV.shift.to_i
-  for n in ARGV
-    result = result / n.to_i unless n.to_i == 0
-  end
-  puts result
+  puts ARGV.reduce { |result, n| result.to_i / n.to_i unless n.to_i == 0 }
 when 'x'
-  result = ARGV.shift.to_i
-  ARGV.each { |n| result *= n.to_i }
-  puts result
+  puts ARGV.reduce(0) { |result, n| result * n.to_i }
 end
